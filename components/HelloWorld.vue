2d6c7faca59d488e8d3e41f81266c68336b0c276
René Zschoch <r.zschoch@me.com>
2025-01-28 16:40:34 +0100

    Use typescript

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -3,8 +3,8 @@ const { myLayer } = useAppConfig();
 </script>
 
 <template>
-  <div>
-    <h1>Hello World!</h1>
-    <pre>{{ myLayer }}</pre>
-  </div>
+	<div>
+		<h1>Hello World!!!</h1>
+		<pre>{{ myLayer }}</pre>
+	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
 	compatibilityDate: "2025-01-24",
+	devtools: { enabled: true },
+	typescript: {
+		typeCheck: true,
+	},
 });
