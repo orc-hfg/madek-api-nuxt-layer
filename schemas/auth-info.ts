a142db54d324a25e4c7cdf08ca4aa68d0f6e86d3
René Zschoch <r.zschoch@me.com>
2025-02-26 12:13:59 +0100

    Fix API request and schema issues, improve robustness

--- schemas/auth-info.ts
+++ schemas/auth-info.ts
@@ -1,5 +1,4 @@
 import type { paths } from '../generated/api/schema';
 
-export type MadekAuthInfoResponse = paths['/api-v2/auth-info']['get']['responses']['200']['schema'];
-export type MadekAuthInfoData = MadekAuthInfoResponse['data'];
-export type AuthInfo = Pick<MadekAuthInfoData, 'id' | 'first_name' | 'last_name'>;
+export type MadekAuthInfoResponse = paths['/api-v2/auth-info']['get']['responses']['200']['schema']['data'];
+export type AuthInfo = Pick<MadekAuthInfoResponse, 'id' | 'login' | 'first_name' | 'last_name'>;
--- server/utils/auth-info.ts
+++ server/utils/auth-info.ts
@@ -1,22 +1,31 @@
 import type { H3Event } from 'h3';
 import { useMadekApi } from '../../composables/useMadekApi';
-import type { MadekAuthInfoResponse, MadekAuthInfoData, AuthInfo } from '../../schemas/auth-info';
+import type { MadekAuthInfoResponse, AuthInfo } from '../../schemas/auth-info';
+import { StatusCodes, getReasonPhrase } from 'http-status-codes';
 
 export const getAuthInfo = async (event: H3Event): Promise<AuthInfo> => {
 	const { fetchFromApi } = useMadekApi(event);
-	const response = await fetchFromApi<MadekAuthInfoResponse>('/auth-info', {
-		needsAuth: true,
-		cache: {
-			maxAge: 60 * 60,
-			swr: false,
-		},
-	});
 
-	const data: MadekAuthInfoData = response.data;
+	try {
+		const response = await fetchFromApi<MadekAuthInfoResponse>('/auth-info', {
+			needsAuth: true,
+			cache: {
+				maxAge: 60 * 60,
+				swr: false,
+			},
+		});
 
-	return {
-		id: data.id,
-		first_name: data.first_name,
-		last_name: data.last_name,
-	};
+		return {
+			id: response.id,
+			login: response.login,
+			first_name: response.first_name,
+			last_name: response.last_name,
+		};
+	} catch (error) {
+		throw createError({
+			statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+			statusMessage: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+			message: 'Failed to fetch auth info',
+		});
+	}
 };
