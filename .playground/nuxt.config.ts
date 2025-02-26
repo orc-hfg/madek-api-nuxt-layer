db9c2b1689b3253259cdc4ef1570dd12ae19283c
René Zschoch <r.zschoch@me.com>
2025-02-26 13:52:00 +0100

    Fix type issue

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -1,15 +1,7 @@
-import { fileURLToPath } from "node:url";
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-	extends: [".."],
-	modules: ["@nuxt/eslint"],
-
-	eslint: {
-		config: {
-			// Use the generated ESLint config for lint root project as well
-			rootDir: fileURLToPath(new URL("..", import.meta.url)),
-		},
-	},
-
-	compatibilityDate: "2025-01-23",
+	devtools: { enabled: true },
+	compatibilityDate: "2025-02-26",
 });
