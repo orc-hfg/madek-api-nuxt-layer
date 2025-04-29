7183941fe8b53b85a8c745f239f7b417f33864a2
René Zschoch <r.zschoch@me.com>
2025-04-29 16:38:51 +0200

    Correctly implement baseURL for api routes

--- app/plugins/madek-api.ts
+++ app/plugins/madek-api.ts
@@ -4,7 +4,7 @@ export default defineNuxtPlugin({
 		// See: https://nuxt.com/docs/guide/recipes/custom-usefetch#custom-fetch
 		const runtimeConfig = useRuntimeConfig();
 		const madekApi = $fetch.create({
-			baseURL: `${runtimeConfig.app.baseURL}/api`,
+			baseURL: `${runtimeConfig.app.baseURL || '/'}api`,
 			// eslint-disable-next-line ts/require-await
 			async onResponseError({ response }) {
 				console.error('Fetch response error:', response);
