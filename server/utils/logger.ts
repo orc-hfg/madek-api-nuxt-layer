6182334b2f1a53a8e4668c7c7fd7c3020be8d844
René Zschoch <r.zschoch@me.com>
2025-07-10 15:36:03 +0200

    Move logger status message before logger initialization

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -14,6 +14,13 @@ export function createLogger(event?: H3Event): Logger {
 	const config = useRuntimeConfig(event);
 	const isDebugLoggingEnabled = Boolean(config.debugLogging);
 
+	if (!hasLoggedStatus) {
+		const statusLogger = createConsola();
+		statusLogger.log(`[Logger] Debug logging is ${isDebugLoggingEnabled ? 'enabled' : 'disabled'}.`);
+
+		hasLoggedStatus = true;
+	}
+
 	// See: https://github.com/unjs/consola?tab=readme-ov-file#log-level
 	const LOG_LEVEL_DEFAULT = 4;
 	const LOG_LEVEL_SILENT = -999;
@@ -21,13 +28,6 @@ export function createLogger(event?: H3Event): Logger {
 	const logger = createConsola({
 		level: isDebugLoggingEnabled ? LOG_LEVEL_DEFAULT : LOG_LEVEL_SILENT,
 	});
-
-	if (!hasLoggedStatus) {
-		const statusLogger = createConsola({ level: 3 });
-		statusLogger.info(`[Logger] Debug logging is ${isDebugLoggingEnabled ? 'enabled' : 'disabled'}.`);
-
-		hasLoggedStatus = true;
-	}
 
 	return {
 		debug(source: string, message: string, data?: unknown): void {
