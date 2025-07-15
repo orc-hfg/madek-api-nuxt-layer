e777586fb2c0a90143e6feea0a02fa00c2f4b8da
René Zschoch <r.zschoch@me.com>
2025-07-15 11:59:53 +0200

    Add debug logging for request headers and host information

--- server/middleware/log-requests.ts
+++ server/middleware/log-requests.ts
@@ -3,5 +3,12 @@ import { createLogger } from '../../shared/utils/logger';
 export default defineEventHandler((event) => {
 	const logger = createLogger(event);
 
+	logger.debug('Middleware: log-requests', 'Headers:', {
+		event,
+		headers: event.node.req.headers,
+		host: event.node.req.headers.host,
+		forwardedHost: event.node.req.headers['x-forwarded-host'],
+	});
+
 	logger.info('Middleware: log-requests', 'Request URL:', getRequestURL(event).href);
 });
